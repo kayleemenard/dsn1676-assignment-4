d78a942e085e24acd320de0658925fd354f6100f
Kaylee Menard <mena0150@algonquinlive.com>
2015-02-11 16:38:04 -0500

    Created repository, added html, js, and css files

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Single line comment
+
+/*
+	Multiple lines are the same as css
+*/	
+
+// alert('Hello!');
+
+
+// String: some text inside quotes
+var firstName = 'Kaylee';
+console.log(firstName);
+
+// Number: no quotes, can be mathified
+var age = 22;
+console.log(age)
+
+// Boolean: true or false
+var isHuman = true;
+
+// Array: a container that holds multiple things
+var dinos = ['Iguanodon', 'Pteranodon', 'T-Rex'];
+console.log(dinos[1]);
+
+// Object: a variable full of other variables
+var trex = {
+	fullName: 'Tyrannosaurus Rex',
+	age: 60000000,
+	isMeatEater: true,
+};
+
+console.log(trex.fullName);
